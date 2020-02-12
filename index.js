1d704c9809adc9a00bb2143aa3c9219b16f84931
IcylFd <772167928@qq.com>
2020-02-12 15:57:24 +0800

    加判断逻辑

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+/*
+ * @Date: 2020-02-11 14:36:39
+ * @LastEditors  : lifangdi
+ * @LastEditTime : 2020-02-12 15:56:57
+ */
 // import caption from './caption'
+const caption = 'Like this? This is not my son, it\'s a fake! How did I come up with this... Father...'
 // js原生方法获取「开始按钮」
 var start = document.getElementById('start-btn')
 
@@ -16,15 +22,26 @@ start.onclick = () => {
   console.log(myPlayer.currentTime())
 }
 
+// 文本框输入事件
+// 每次键盘输入事件，获取文本框的值，并进判断逻辑
+var n = 0
 input.oninput = () => {
   var value = input.value
   // var n = value.length
-  var n = 0
-  if(value) {
+
+
+  ///////// 差这里判断逻辑
+  if(caption.includes(value)) {
     n++
+    console.log('true', n);
+    // 控制输入每个字符的播放时间
+    n < 10
+    ? myPlayer.currentTime(0.414 + n * 0.07)
+    : myPlayer.currentTime(0.414 + n * 0.08)
+  } else {
+    n--
+    console.log('false', n);
+    myPlayer.pause()
   }
-  // 控制输入每个字符的播放时间
-  n < 10
-  ? myPlayer.currentTime(0.414 + n * 0.07)
-  : myPlayer.currentTime(0.414 + n * 0.08)
+  
 }
