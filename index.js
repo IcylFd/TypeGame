94e953a4d3b3f9c8a7ee4f9bd943c859106a8ec1
IcylFd <772167928@qq.com>
2020-02-12 20:49:24 +0800

    修改判断逻辑；加了些注释

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-02-11 14:36:39
  * @LastEditors  : lifangdi
- * @LastEditTime : 2020-02-12 15:56:57
+ * @LastEditTime : 2020-02-12 20:48:13
  */
 // import caption from './caption'
 const caption = 'Like this? This is not my son, it\'s a fake! How did I come up with this... Father...'
@@ -19,29 +19,43 @@ start.onclick = () => {
   myPlayer.play()
   myPlayer.currentTime(0.414)
   myPlayer.pause()
-  console.log(myPlayer.currentTime())
+}
+
+// 控制输入每个字符的播放时间
+// 这里是粗略比对，第一句话每帧大概0.07s，后面每帧大概0.08s
+const pre = (n) => {
+  n < 10
+  ? myPlayer.currentTime(0.414 + n * 0.07)
+  : myPlayer.currentTime(0.414 + n * 0.08)
+}
+
+const back = (n) => {
+  var curTime = myPlayer.currentTime()
+  n < 10
+  ? myPlayer.currentTime(curTime - 0.07)
+  : myPlayer.currentTime(curTime - 0.08)
+
 }
 
 // 文本框输入事件
 // 每次键盘输入事件，获取文本框的值，并进判断逻辑
-var n = 0
-input.oninput = () => {
+var n = 0   // 标志变量n
+input.oninput = (e) => {
   var value = input.value
-  // var n = value.length
+  var caption_arr = caption.split('')
 
-
-  ///////// 差这里判断逻辑
-  if(caption.includes(value)) {
-    n++
-    console.log('true', n);
-    // 控制输入每个字符的播放时间
-    n < 10
-    ? myPlayer.currentTime(0.414 + n * 0.07)
-    : myPlayer.currentTime(0.414 + n * 0.08)
-  } else {
-    n--
-    console.log('false', n);
-    myPlayer.pause()
-  }
-  
+  // 判断是否按del键
+  if(e.data) {
+    // 比对每个字母是否正确的逻辑
+    if(caption_arr[value.length - 1] === e.data) {
+      n++
+      pre(n)
+    } else {
+      myPlayer.pause()
+    }
+  } else if (n > value.length) {
+      // 删除到正确的字母回退视频逻辑
+      back(n)
+      n--
+    }
 }
