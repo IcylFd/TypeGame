a22d71c152925ddefbd391676ca5769715d959f4
IcylFd <772167928@qq.com>
2020-02-13 20:44:04 +0800

    index.css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-02-11 14:36:39
  * @LastEditors  : lifangdi
- * @LastEditTime : 2020-02-12 22:08:55
+ * @LastEditTime : 2020-02-13 20:05:30
  */
 const CAPTION = 'Like this? This is not my son, it\'s a fake! How did I come up with this... Father...'
 // js原生方法获取「开始按钮」
@@ -10,14 +10,16 @@ const start = document.getElementById('start-btn')
 // js原生方法获取「文本框」
 const input = document.getElementById('input')
 
+const game_tip = document.getElementById('game-tip')
+
 // 通过id实例化videojs对象
 const myPlayer = videojs('my-video')
 
 // 绑定「开始按钮」点击事件
 start.onclick = () => {
-  myPlayer.play()
   myPlayer.currentTime(0.414)
-  myPlayer.pause()
+  start.classList.add('remove-start')
+  input.focus();
 }
 
 // 控制输入每个字符的播放时间
@@ -47,14 +49,25 @@ input.oninput = (e) => {
   if(e.data) {
     // 比对每个字母是否正确的逻辑
     if(caption_arr[value.length - 1] === e.data) {
+      console.log(1)
       n++
       pre(n)
+
+      // 成功结束逻辑
+      if(value.length === CAPTION.length) {
+        game_tip.classList.remove('display-end')
+        game_tip.classList.remove('show-end')
+        input.blur()
+      }
     } else {
+      console.log(2)
       myPlayer.pause()
     }
   } else if (n > value.length) {
-      // 删除到正确的字母回退视频逻辑
-      back(n)
-      n--
-    }
+    console.log(3)
+
+    // 删除到正确的字母回退视频逻辑
+    back(n)
+    n--
+  }
 }
